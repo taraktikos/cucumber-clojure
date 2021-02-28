d8cf190c4c5c152312948e14c28acad9d3d9cf76
GitHub Action <action@github.com>
2021-02-28 20:10:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/taraktikos/cucumberclojure/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.taraktikos.cucumberclojure.services
+
+import com.github.taraktikos.cucumberclojure.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
